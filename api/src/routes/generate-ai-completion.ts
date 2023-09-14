c67c6620c3a1fbe7816f6c7aa905b506cba905bf
GabrielLicar <paulogabrielicar@gmail.com>
2023-09-14 11:49:23 -0300

    :stars: project finalized

--- api/src/routes/generate-ai-completion.ts
+++ api/src/routes/generate-ai-completion.ts
@@ -1,3 +1,4 @@
+import { OpenAIStream, streamToResponse } from 'ai';
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { openai } from '../lib/openai';
@@ -8,11 +9,11 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
   app.post('/ai/complete', async (request, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     })
 
-    const { template, videoId, temperature } = bodySchema.parse(request.body)
+    const { prompt, videoId, temperature } = bodySchema.parse(request.body)
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -22,7 +23,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 
     if (!video.transcription) return reply.status(400).send({ error: 'Video transcription was not generated yet.' })
 
-    const promptMessage = template.replace('{transcription}', video.transcription);
+    const promptMessage = prompt.replace('{transcription}', video.transcription);
 
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo-16k',
@@ -30,8 +31,16 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
       messages: [
         { role: 'user', content: promptMessage },
       ],
+      stream: true,
     })
 
-    return response
+    const stream = OpenAIStream(response);
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      }
+    });
   })
 }
